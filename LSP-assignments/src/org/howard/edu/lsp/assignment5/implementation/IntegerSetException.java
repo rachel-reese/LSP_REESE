182ad52f16003366297fb1d2fd1549e745f4a4c0
rachel-reese <rachel.reese@bison.howard.edu>
2022-03-04 23:19:11 -0600

    Revert "Revert "assignment 5""
    This reverts commit 7b5b47a724f83d6f669e4d89468abab170f20e42.

---
+++ LSP-assignments/src/org/howard/edu/lsp/assignment5/implementation/IntegerSetException.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lsp.assignment5.implementation;
+
+public class IntegerSetException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public IntegerSetException(String errorMsg) {
+		super(errorMsg);
+	}
+
+}
